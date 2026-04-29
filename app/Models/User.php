4792a79c8c915dd57b8a0bda9f71afaed4f48465
Gio <gio@gtc.co>
2026-04-28 17:55:46 -0700

    Adding needed fillable and hidden attributes

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+
+#[Fillable(['name', 'email', 'password'])]
+#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     use HasApiTokens, HasRoles, HasFactory, Notifiable;
