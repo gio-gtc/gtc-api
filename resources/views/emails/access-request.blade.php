72f7b7ec0305c026ece4f87c29ff5b185aadeeb1
Gio <gio@gtc.co>
2026-05-06 17:13:27 -0700

    Updating email with 2 buttons - needs test

--- resources/views/emails/access-request.blade.php
+++ resources/views/emails/access-request.blade.php
@@ -5,15 +5,39 @@ You have received a new request to access the platform.
 
 **Name:** {{ $data['first_name'] }} {{ $data['last_name'] }}<br>
 **Email:** {{ $data['email'] }}<br>
-**Company:** {{ $data['company'] }}<br>
+**Organisation:** {{ $data['organisation'] }}<br>
 **Job Title:** {{ $data['job_title'] }}<br>
 **Phone:** {{ $data['phone'] ?? 'N/A' }}
 
 **Why they need access:**
 > {{ $data['details'] }}
 
-<x-mail::button :url="'mailto:' . $data['email']">
-Reply to {{ $data['first_name'] }}
+@php
+    // 1. Grab your proxy's URL from the .env (fallback to localhost for testing)
+    $frontendUrl = env('FRONTEND_URL', 'http://localhost:8000');
+    
+    // 2. Build the query string with the user's data
+    $queryParams = http_build_query([
+        'action' => 'create_contact',
+        'first_name' => $data['first_name'],
+        'last_name' => $data['last_name'],
+        'email' => $data['email'],
+        'organisation' => $data['organisation'],
+        'job_title' => $data['job_title'],
+        'phone' => $data['phone'] ?? '',
+    ]);
+
+    // 3. Combine them to make the final link
+    // Change '/contacts' to whatever your actual route is for the user management page!
+    $createAccountUrl = $frontendUrl . '/dashboard?' . $queryParams;
+@endphp
+
+<x-mail::button :url="'mailto:' . $data['email'] . '?subject=GTC%20Force%20New%20Access%20Request'">
+Reply
+</x-mail::button>
+
+<x-mail::button :url="$createAccountUrl" color="success">
+Create Account
 </x-mail::button>
 
 Thanks,<br>
