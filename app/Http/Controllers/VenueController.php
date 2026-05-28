46018b254f0872e2c0c29eb9040576c697f8117c
Gio <gio@gtc.co>
2026-05-27 19:16:48 -0700

    Adding search and limit to venues api

--- app/Http/Controllers/VenueController.php
+++ app/Http/Controllers/VenueController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Venue;
+use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 
 class VenueController extends Controller
@@ -10,10 +11,28 @@ class VenueController extends Controller
     /**
      * Display a listing of all available venues.
      */
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
+        $search = $request->query('search');
+
+        if ($search && strlen($search) < 2) {
+            return response()->json(['venues' => []], 200);
+        }
+
+        $venues = Venue::query()
+            ->with('country:id,name') 
+            ->when($search, function ($query, $search) {
+                $query->where(function ($q) use ($search) {
+                    $q->where('name', 'like', "%{$search}%")
+                        ->orWhere('city', 'like', "%{$search}%")
+                        ->orWhere('state', 'like', "%{$search}%");
+                });
+            })
+            ->limit(10) 
+            ->get(['id', 'name', 'city', 'state', 'country_id']); 
+
         return response()->json([
-            'venues' => Venue::orderBy('name')->get()
+            'venues' => $venues
         ], 200);
     }
 }
