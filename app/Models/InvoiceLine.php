4d42c42650c3c030df0fb634d9d09a263cfa92f9
Gio <gio@gtc.co>
2026-06-01 10:37:13 -0700

    Finished creating connetion to invoice and invoiceLine models

--- app/Models/InvoiceLine.php
+++ app/Models/InvoiceLine.php
@@ -5,11 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class InvoiceLine extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['invoice_id', 'order_item_id', 'description', 'price'];
+    protected $fillable = [
+        'invoice_id', 
+        'order_item_id', 
+        'description', 
+        'price'
+    ];
+
+    public function invoice(): BelongsTo
+    {
+        return $this->belongsTo(Invoice::class);
+    }
+
+    public function orderItem(): BelongsTo
+    {
+        return $this->belongsTo(OrderItem::class);
+    }
 }
